6c296b5627262a3d7d9f0956d6bf41107a5cc54e
Ivan Meleshko <ivanmeleshko@gmail.com>
2022-06-14 13:19:33 +0300

    Bishop class

---
+++ Chess/src/com/skillfactory/chess/Bishop.java
@@ -0,0 +1,40 @@
+package com.skillfactory.chess;
+
+public class Bishop extends ChessPiece {
+
+	public Bishop(String color) {
+		super(color);
+	}
+
+	@Override
+	public String getColor() {
+		return color;
+	}
+
+	@Override
+	public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+		boolean result = false;
+		if (checkPos(line) && checkPos(column) && checkPos(toLine) && checkPos(toColumn) && 
+				chessBoard.board[line][column] != null) {
+			for (int i = 1; i < 8; i++) {
+				if (((line - toLine == i) && (column - toColumn == i)) ||  // top left
+						((line - toLine == i) && (toColumn - column == i)) ||  // top right
+						((toLine - line == i) && (toColumn - column == i)) ||  // bottom right
+						((toLine - line == i) && (column - toColumn == i))) {   // bottom left
+							result = true;
+						}	
+			}
+		}
+		return result;
+	}
+
+	@Override
+	public String getSymbol() {
+		return "B";
+	}
+	
+	public boolean checkPos(int x) {
+		return (x >= 0 && x <= 7);
+	}
+
+}
--- Chess/src/com/skillfactory/chess/Main.java
+++ Chess/src/com/skillfactory/chess/Main.java
@@ -8,10 +8,13 @@ public class Main {
 		ChessBoard myChessboard = new ChessBoard();
 		myChessboard.board[0][0] = new Horse("Black");
 		myChessboard.board[3][3] = new Horse("Black");
+		myChessboard.board[1][1] = new Bishop("White");
 		
 		myChessboard.board[2][6] = new Horse("Black");
 		myChessboard.canMoveToPosition(0, 0, 2, 1);
 		myChessboard.canMoveToPosition(2, 1, 0, 0);
+		myChessboard.canMoveToPosition(1, 1, 5, 5);
+		
 		myChessboard.printBoard2();
 		
 	}
