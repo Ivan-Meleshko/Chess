f73bccd545b3566b5658a9c5bc3809a95da5146c
Ivan Meleshko <ivanmeleshko@gmail.com>
2022-06-14 12:43:57 +0300

    Pawn class

--- Chess/src/com/skillfactory/chess/Pawn.java
+++ Chess/src/com/skillfactory/chess/Pawn.java
@@ -11,19 +11,41 @@ public class Pawn extends ChessPiece {
 		return color;
 	}
 
-	@Override
-	public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-		
-		if (checkPos(line) && checkPos(column) && checkPos(toLine) && checkPos(toColumn) && chessBoard.board[line][column] != null) {
-			
-			int dirCoef;
-			int startPos;
-		}
-			return false;
-	
-		
-	}
+	 @Override
+	    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+	        if (checkPos(line) && checkPos(column) && checkPos(toLine) && checkPos(toColumn) && chessBoard.board[line][column] != null) { // check that position in board
+	            if (column == toColumn) { 
+	                int dir;
+	                int start;
 
+	                if (color.equals("White")) {  
+	                    dir = 1;
+	                    start = 1;
+	                } else { 
+	                    dir = -1;
+	                    start = 6;
+	                }
+
+	                if (line + dir == toLine) {  
+	                    return chessBoard.board[toLine][toColumn] == null;
+	                }
+
+	                if (line == start && line + 2 * dir == toLine) {
+	                    return chessBoard.board[toLine][toColumn] == null && chessBoard.board[line + dir][column] == null; // check that positions is null
+	                }
+
+	            } else { // want to eat piece
+
+	                if ((column - toColumn == 1 || column - toColumn == -1) && (line - toLine == 1 || line - toLine == -1) && // check that piece another color
+	                        chessBoard.board[toLine][toColumn] != null) {
+	                    return !chessBoard.board[toLine][toColumn].getColor().equals(color);
+	                } else return false;
+	            }
+	        }
+	        return false;
+	 }
+	 
+	       
 	@Override
 	public String getSymbol() {
 		// TODO Auto-generated method stub
